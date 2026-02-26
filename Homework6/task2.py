4fa037aba7da7d2795900c459e16a39d0c6c86f6
Elisaveta <lmalyutka@mail.ru>
2026-02-26 18:40:43 +0300

    Задания выполнены

--- Homework6/task2.py
+++ Homework6/task2.py
@@ -5,11 +5,24 @@
 # local_function должна изменить значение переменной msg на значение 2
 
 def global_function():
-  msg = 1
-  def local_function():
-  # Здесь нужно написать код
-   
-  return msg
+    """
+    Глобальная функция с локальной переменной и локальной функцией
+    :return:
+    """
+    msg = 1
+
+    def local_function():
+        """
+        Локальная функция, которая меняет локальную функцию.
+        Объемлющая область видимости
+        """
+        # Здесь нужно написать код
+        nonlocal msg
+        msg = 2
+
+    local_function()
+
+    return msg
 
 assert global_function() == 2, 'Значение переменной msg должно быть равно 2'
 print('Все ок')
